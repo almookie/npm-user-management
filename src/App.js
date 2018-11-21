ca5fb2e66b907d367b327aff28b341247a15514d
Ali Mokdad <al_mookie@hotmail.com>
2018-11-21 15:50:07 +1100

    Created dynamic fieldset component to allow addition of fields with same name

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // required imports
 import React, { Component } from 'react';
-import { Form, Card, Row, Col, Input, Button, Select, Radio, Checkbox } from 'antd';
+import { Form, Card, Row, Col, Input, Button, Select, Radio, Checkbox, Icon } from 'antd';
 import 'antd/dist/antd.css';
 import logo from './logo.svg';
 import './App.scss';
@@ -14,36 +14,127 @@ const RadioButton = Radio.Button;
 const CheckboxGroup = Checkbox.Group;
 const CheckboxButton = Checkbox.Button;
 
-// Website Form structure
-class WebsiteForm extends Component {    
+// Mobile Responsive Layouts
+const formItemLayout = {
+    labelCol: {
+        xs: { span: 24 },
+        sm: { span: 3 },
+    },
+    wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 21 },
+    },
+};
+const formItemLayoutWithOutLabel = {
+  wrapperCol: {
+    xs: { span: 24, offset: 0 },
+    sm: { span: 20, offset: 4 },
+  },
+};
+const formTailLayout = {
+    wrapperCol: {
+        xs: {
+            span: 24,
+            offset: 0,
+        },
+        sm: {
+            span: 21,
+            offset: 3,
+        },
+    },
+};
 
+
+// Dynamic field container allows multiple fields to be created that have the same name and layout
+class DynamicFieldSet extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    add = () => {
+        const { name, form } = this.props;
+        const keys = form.getFieldValue(name);
+        const newKeys = keys.concat(keys.length);
+        form.setFieldsValue({
+            [name]: newKeys
+        }); 
+    }
+
+    remove = (k) => {
+        const { name, form } = this.props;
+        const keys = form.getFieldValue(name);
+        
+        if(keys.length === 1) return;
+        
+        form.setFieldsValue({
+            [name]: [name].filter(key => key !== k)
+        });
+    }
 
     render() {
-        const { getFieldDecorator } = this.props.form;
+        const { name, form } = this.props;
+        form.getFieldDecorator(name, { initialValue: [] });
+        const keys = form.getFieldValue(name);
+        const fields = keys.map((k, index) => {
+            return (
+                <FormItem key={k}>
+                    {
+                        form.getFieldDecorator(`names[${k}]`, {
+                            validateTrigger: ['onChange', 'onBlur'],
+                            rules: [{
+                                required: true,
+                                whitespace: true,
+                                message: "Please input passenger's name or delete this field.",
+                            }],
+                        })
+                        (
+                            <TextArea placeholder="Reference Website" style={{ width: '60%', marginRight: 8 }} />
+                        )
+                    }
+                    {
+                        keys.length &&
+                            <Icon className="dynamic-delete-button" type="minus-circle-o" disabled={keys.length === 1} onClick={() => this.remove(k)} />
+                    }
+                </FormItem>
+            )
+        });
 
-        // Mobile Responsive Layouts
-        const formItemLayout = {
-            labelCol: {
-                xs: { span: 24 },
-                sm: { span: 3 },
-            },
-            wrapperCol: {
-                xs: { span: 24 },
-                sm: { span: 21 },
-            },
-        };
-        const formTailLayout = {
-            wrapperCol: {
-                xs: {
-                    span: 24,
-                    offset: 0,
-                },
-                sm: {
-                    span: 21,
-                    offset: 3,
-                },
-            },
-        };
+        return (
+            <>
+                {fields}
+                <FormItem {... formItemLayoutWithOutLabel}>
+                    <Button type="dashed" onClick={this.add} style={{ width: '60%' }}>
+                        <Icon type="plus"/> Add Reference
+                    </Button>
+                </FormItem>
+            </>
+        );
+    }
+}
+
+// Website Form structure
+class WebsiteForm extends Component {     
+    addCompetitorWebsite = () => {
+        const { form } = this.props;
+        const keys = form.getFieldValue('keys');
+        const newKeys = keys.concat(keys.length);
+        form.setFieldsValue({
+            keys: newKeys
+        });
+    }
+    removeCompetitorWebsite = (k) => {
+        const { form } = this.props;
+        const keys = form.getFieldValue('keys');
+
+        if (keys.length === 1) return;
+
+        form.setFieldsValue({
+            keys: keys.filter(key => key !== k)
+        });
+    }
+
+    render() {
+        const { getFieldDecorator, getFieldValue } = this.props.form;
 
         // variables to reduce clutter in render return
         // Target Audience
@@ -66,6 +157,33 @@ class WebsiteForm extends Component {
         const websiteFeatures = ["Easy to update by non-technical people", "Shopping cart or other ecommerce", "Feedback/contact forms", "Newsletters and signup", "Animation", "Calendar", "Members only section", "Faster downloads", "Business process streamlining", "Film/Audio", "High google (and other searches) ranking", "Optimisation for mobile phones", "Email marketing", "Photo and media galleries", "Statistics", "Surveys", "Film", "Blog"]
         const websiteFeaturesCheckboxes = websiteFeatures.map(key => <Checkbox key={key} value={key}>{key}</Checkbox>);
 
+        // Competitor Websites
+        getFieldDecorator('keys', { initialValue: [] });
+        const keys = getFieldValue('keys');
+        const competitorWebsites = keys.map((k, index) => {
+            return (
+                <FormItem key={k}>
+                    {
+                        getFieldDecorator(`names[${k}]`, {
+                            validateTrigger: ['onChange', 'onBlur'],
+                            rules: [{
+                                required: true,
+                                whitespace: true,
+                                message: "Please input passenger's name or delete this field.",
+                            }],
+                        })
+                        (
+                            <TextArea placeholder="Reference Website" style={{ width: '60%', marginRight: 8 }} />
+                        )
+                    }
+                    {
+                        keys.length &&
+                            <Icon className="dynamic-delete-button" type="minus-circle-o" disabled={keys.length === 1} onClick={() => this.removeCompetitorWebsite(k)} />
+                    }
+                </FormItem>
+            )
+        });
+    
         return (
             <div className="form-canvas">
                 <Form>
@@ -120,11 +238,8 @@ class WebsiteForm extends Component {
                         <h2>Competitor Websites</h2>
                         <div className="box">
                             <div className="callout">Please outline reference websites and what you like / dislike about their sites</div>
-                            <Input addonBefore="https://" defaultValue="aiims.com.au"/>
-                            <FormItem {...formItemLayout} label="Reference">
-                                <TextArea rows={2}></TextArea>
-                                {/* TODO: Dynamically add more references */}
-                            </FormItem>
+                            <Input addonBefore="https://" defaultValue="aiims.com.au"/> 
+                            <DynamicFieldSet name="competitorWebsites" form={this.props.form}/>
                         </div>
                     </div>
                 </Form>
